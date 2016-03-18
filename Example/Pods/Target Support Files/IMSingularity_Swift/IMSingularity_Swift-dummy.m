acb887ba4b6c31165b37ceda51567f07f907c74e
Mqhong <617257112@qq.com>
2016-03-18 13:20:13 +0800

    ddd
    Signed-off-by: Mqhong <617257112@qq.com>

---
+++ Example/Pods/Target Support Files/IMSingularity_Swift/IMSingularity_Swift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IMSingularity_Swift : NSObject
+@end
+@implementation PodsDummy_IMSingularity_Swift
+@end
---
+++ Example/Pods/Target Support Files/IMSingularity_Swift/IMSingularity_Swift-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double IMSingularity_SwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char IMSingularity_SwiftVersionString[];
+
